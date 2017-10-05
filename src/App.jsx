50138ec137c83d00893784911db6201e33a27431
Jeff FitzGerald <j33ff.fitzgerald@gmail.com>
2017-10-05 23:36:40 +0000

    connected websockets

--- src/App.jsx
+++ src/App.jsx
@@ -53,21 +53,18 @@ class App extends Component {
 }
 
 componentDidMount() {
-  console.log("componentDidMount <App />");
-  setTimeout(() => {
-    console.log("Simulating incoming message");
-    // Add a new message to the list of messages in the data store
-    const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-    const messages = this.state.messages.concat(newMessage)
-    // Update the state of the app component.
-    // Calling setState will trigger a call to render() in App and all child components.
-    this.setState({messages: messages})
-  }, 3000);
+ this.socket = new WebSocket("ws://localhost:3001");
+
 }
 
-addMessage(message) {
- console.log(message);
+
+addMessage = (message)  => {
+ console.log(typeof message);
+ const newMessage = {username: currentUser.username, content: currentUser.content};
+ const messages = this.state.messages.concat(newMessage);
+ this.setState({messages:messages})
 }
+
 
     render(){
     // console.log("Rendering <App/>");
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -4,13 +4,19 @@ class ChatBar extends Component {
   constructor(props){
     super(props);
   }
-
+Keydown(event){
+  if (event.key == 'enter'){
+    event.preventDefault();
+    this.props.addMessage(event.target.value);
+    event.target.value= "";
+  }
+}
   render(){
     return (
 
     <footer className="chatbar">
     <input className="chatbar-username" defaultValue={this.props.user.name} placeholder="Your Name (Optional)" />
-    <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+    <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyDown={this.keyDown}/>
     </footer>
       )
   }
